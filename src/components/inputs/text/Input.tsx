799aedd169850f778e8e0705742d87806c0ba361
Floriian <lovasifloriian@outlook.hu>
2024-07-10 17:56:47 +0200

    create input component

--- src/components/inputs/text/Input.tsx
+++ src/components/inputs/text/Input.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import "./Input.scss";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  errorText?: string;
+  errorText?: FieldError | undefined;
 }
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, errorText, label, ...props }, ref) => {
     return (
-      <div ref={ref}>
-        {label && (
-          <label htmlFor={props.id} className={className}>
-            {label}
-          </label>
-        )}
+      <div ref={ref} className="input-wrapper">
+        {label && <label htmlFor={props.id}>{label}</label>}
         <input {...props} />
-        {errorText && <p>{errorText}</p>}
+        {errorText && <p>{errorText.message}</p>}
       </div>
     );
   }
