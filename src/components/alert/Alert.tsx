f9c8f677556a845e30fc3b9ac83297e24052df7d
Floriian <lovasifloriian@outlook.hu>
2024-07-10 20:35:20 +0200

    use mui icons instead of 'X'

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import "./Alert.scss";
 
@@ -9,7 +10,9 @@ export const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
   ({ className, ...props }, ref) => (
     <div ref={ref} className="alert">
       <div>{props.children}</div>
-      <div onClick={props.onIconClick}>X</div>
+      <span className="material-symbols-outlined" onClick={props.onIconClick}>
+        close
+      </span>
     </div>
   )
 );
